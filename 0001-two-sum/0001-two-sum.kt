ac9bc2aee98f2d4f7482c69581ed755335829809
Neil Narnaware <neilnarnaware123@gmail.com>
2024-12-25 00:44:19 -0500

    Time: 54 ms (12.41%), Space: 39 MB (46.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -2,19 +2,27 @@ class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         
         /*
-
+        a + b = target
+        3 + 2 != 6
+        2 + 4 = 6
          */
-        
+            
         val arr = IntArray(2)
-        for(i in 0 until nums.size){
-            for(j in 0 until nums.size){
-                if(nums[i] + nums[j] == target && i != j){
-                    arr[0] = i
-                    arr[1] = j
-                }
+        var left = 0
+        var right = nums.size-1
+        while(left < nums.size ){
+            if(nums[left] + nums[right] == target && left != right){
+                arr[0] = left
+                arr[1] = right
+                break
             }
+            if(right == left){
+                left++
+                right = nums.size-1
+            }else{            
+                right--
+            } 
         }
-
         return arr
     }
 }
